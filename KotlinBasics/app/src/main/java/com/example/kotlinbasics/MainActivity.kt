2ba90de4254156484fb193c8b8f84e45fd930e1e
Mohammad Kamrul Hasan <mkhrussell@gmail.com>
2017-07-06 01:13:04 +0600

    Mutable List and Immutable List

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/MainActivity.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/MainActivity.kt
@@ -14,10 +14,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val countries: Array<String> = arrayOf("Bangladesh", "India", "Pakistan", "Srilanka", "Bhutan", "Nepal", "Afganistan", "Maldives")
-        val adapter = ArrayAdapter<String>(applicationContext, android.R.layout.simple_list_item_1, countries)
+        var myList: List<String> = listOf("John", "Russell", "Mac") // Immutable
+        var myList2: List<String> = listOf<String>("Dhaka", "Khulna", "Tangail") // Immutable
 
-        val lstCountries = findViewById(R.id.lstCountries) as ListView
-        lstCountries.adapter = adapter
+        var mutableList: MutableList<String> = mutableListOf("Bangladesh", "India", "Nepal") // Mutable
+        mutableList[1] = "Pakistan"
+        mutableList.add("Srilanka")
+
+        var arrayList = ArrayList<String>()
+        arrayList.add("Apple")
+        arrayList.add("Orange")
+        arrayList[0] = "Pitch"
+
+//        for(index in 0..mutableList.size - 1) {
+//            Log.d(TAG, "${mutableList[index]}")
+//        }
+
+//        for(item in arrayList) {
+//            Log.d(TAG, "${item}")
+//        }
+
+//        val countries: Array<String> = arrayOf("Bangladesh", "India", "Pakistan", "Srilanka", "Bhutan", "Nepal", "Afganistan", "Maldives")
+//        val adapter = ArrayAdapter<String>(applicationContext, android.R.layout.simple_list_item_1, countries)
+//
+//        val lstCountries = findViewById(R.id.lstCountries) as ListView
+//        lstCountries.adapter = adapter
     }
 }
