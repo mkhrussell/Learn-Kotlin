0f0e454ade1f00a3ef6fac08b1c6282b2922e1c2
Mohammad Kamrul Hasan <mkhrussell@gmail.com>
2017-07-09 18:50:00 +0600

    Populate ListView using Mutable List

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/MainActivity.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.example.kotlinbasics
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.support.v7.app.AppCompatActivity
 import android.widget.ArrayAdapter
+import android.widget.EditText
 import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
@@ -14,30 +14,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var myList: List<String> = listOf("John", "Russell", "Mac") // Immutable
-        var myList2: List<String> = listOf<String>("Dhaka", "Khulna", "Tangail") // Immutable
+        val countries = mutableListOf<String>("Bangladesh", "India") //, "Pakistan", "Srilanka", "Bhutan", "Nepal", "Afganistan", "Maldives")
+        val adapter = ArrayAdapter<String>(applicationContext, android.R.layout.simple_list_item_1, countries)
 
-        var mutableList: MutableList<String> = mutableListOf("Bangladesh", "India", "Nepal") // Mutable
-        mutableList[1] = "Pakistan"
-        mutableList.add("Srilanka")
+        val lstCountries = findViewById(R.id.lstCountries) as ListView
+        lstCountries.adapter = adapter
 
-        var arrayList = ArrayList<String>()
-        arrayList.add("Apple")
-        arrayList.add("Orange")
-        arrayList[0] = "Pitch"
+        var edtCountry = findViewById(R.id.edtCountry) as EditText
+        findViewById(R.id.btnAdd).setOnClickListener {
+            var countryValue = edtCountry.text.toString()
 
-//        for(index in 0..mutableList.size - 1) {
-//            Log.d(TAG, "${mutableList[index]}")
-//        }
-
-//        for(item in arrayList) {
-//            Log.d(TAG, "${item}")
-//        }
-
-//        val countries: Array<String> = arrayOf("Bangladesh", "India", "Pakistan", "Srilanka", "Bhutan", "Nepal", "Afganistan", "Maldives")
-//        val adapter = ArrayAdapter<String>(applicationContext, android.R.layout.simple_list_item_1, countries)
-//
-//        val lstCountries = findViewById(R.id.lstCountries) as ListView
-//        lstCountries.adapter = adapter
+            if (!countryValue.isNullOrBlank()) {
+                adapter.add(countryValue)
+                adapter.notifyDataSetChanged()
+                edtCountry.setText("")
+            }
+        }
     }
 }
