dc1be69fd3862c3eaecb46df21551710469afe28
Mohammad Kamrul Hasan <mkhrussell@gmail.com>
2017-07-10 22:58:46 +0600

    Kotlin function basics

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/MainActivity.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/MainActivity.kt
@@ -9,29 +9,56 @@ class MainActivity : AppCompatActivity() {
     private val TAG: String = javaClass.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate: starts")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        var nonNullName = "Russell"
-//        var nullableName: String? = "Russell"
-//        nullableName = null
-//        var nonNullVar2 = "John"
-//        nonNullName = nullableName ?: nonNullVar2 // Elvis operator
+        var resutlt1 = sum(20, 30) // 50
+        Log.d(TAG, "onCreate: sum of 20 and 30 is: $resutlt1")
 
-//        var countryName: String? = null
-//        countryName = "Bangladesh"
-//        Log.d(TAG, "onCreate: " + countryName?.toUpperCase()) // good way
-//        Log.d(TAG, "onCreate: " + countryName!!.toUpperCase()) // Dangerous
+        // Print only
+        sumAndPrint(40, 50) // 90
 
-        /*
-        if(countryName != null) {
-            Log.d(TAG, "onCreate: " + countryName.toUpperCase())
-        }
-        */
+        // Print only
+        sumAndPrint2(40, 50) // 90
 
-        var intValue = 8000000
-        var strValue = intValue as? String
+        // Named parameters
+        var result2 = sum(b = 60, a = 20) // 80
+        Log.d(TAG, "onCreate: sum of 20 and 60 is: $result2")
 
-        Log.d(TAG, "onCreate: " + strValue?.toUpperCase())
+        var intA = 70
+        var intB = 20
+        Log.d(TAG, "onCreate: sum of $intA and $intB is: ${sum(b = intA, a = intB)}") // 90
+
+        Log.d(TAG, "onCreate: max of 50 and 20 is: ${max(50, 20)}")
+
+        Log.d(TAG, "onCreate: max of 50 and 20 is: ${max2(50, 20)}")
+
+        Log.d(TAG, "onCreate: ends")
     }
+
+    // Function with return value
+    fun sum(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    // Function without return value
+    fun sumAndPrint(a: Int, b: Int): Unit {
+        var sum = a + b
+        Log.d(TAG, "sumAndPrint: sum of $a and $b is: $sum")
+    }
+
+    // Function without return value
+    fun sumAndPrint2(a: Int, b: Int) {
+        var sum = a + b
+        Log.d(TAG, "sumAndPrint2: sum of $a and $b is: $sum")
+    }
+
+    fun max(a: Int, b: Int): Int {
+        if (a > b) return a
+        else return b
+    }
+
+    // Function expression
+    fun max2(a: Int, b: Int): Int = if (a > b) a else b
 }
