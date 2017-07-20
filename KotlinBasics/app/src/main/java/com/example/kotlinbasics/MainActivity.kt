7b1b4b94937c0db73c1281763bb830c5f8c96a2b
Mohammad Kamrul Hasan <mkhrussell@gmail.com>
2017-07-20 22:52:45 +0600

    Interface: has-a relationship

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/MainActivity.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/MainActivity.kt
@@ -13,37 +13,85 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var shapeOne: Shape = Circle(0, 0)
-        var shapeTwo: Shape = Rectangle(100, 100)
+        var dog: Dog = Dog()
+        dog.bark()
 
-        shapeOne.draw()
-        shapeTwo.draw()
+        var human: Human = Human()
+        Log.d(LOG_TAG, "I am ${human.javaClass.simpleName}, I have life.")
+
+        var robot: Robot = Robot()
+        var talkingRobot = TalkingRobot()
+
+        var moveable: Moveable = dog
+        moveable.move()
+        moveable = human
+        moveable.move()
+        moveable = robot
+        moveable.move()
+        moveable = talkingRobot
+        moveable.move()
+
+        var eatable: Eatable = dog
+        eatable.eat()
+        eatable = human
+        eatable.eat()
+
+        var speakable: Speakable = human
+        speakable.speak()
+        speakable = talkingRobot
+        speakable.speak()
+
 
         Log.d(TAG, "onCreate: ends")
     }
 }
 
-var DRAW_TAG: String = "SampleDrawing"
+val LOG_TAG = "DemoLogs"
 
-
-// IS-A relationship
-
-abstract class Shape(xPos: Int, yPos: Int) {
-    var x: Int = xPos
-    var y: Int = yPos
-
-    abstract fun draw()
+interface Moveable {
+    fun move()
 }
 
-class Circle(xPos: Int, yPos: Int) : Shape(xPos, yPos) {
-    override fun draw() {
-        Log.d(DRAW_TAG, "Drawing circle using circle drawing formula at position (${x}, ${y}).")
+interface Eatable {
+    fun eat()
+}
+
+interface Speakable {
+    fun speak()
+}
+
+abstract class Animal : Moveable, Eatable {
+    val hasLife: Boolean = true
+
+    override fun move() {
+        Log.d(LOG_TAG, "I am ${javaClass.simpleName}, I can move.")
+    }
+
+    override fun eat() {
+        Log.d(LOG_TAG, "I am ${javaClass.simpleName}, I can eat.")
     }
 }
 
-class Rectangle(xPos: Int, yPos: Int) : Shape(xPos, yPos) {
-    override fun draw() {
-        Log.d(DRAW_TAG, "Drawing rectangle using rectangle drawing formula at position (${x}, ${y}).")
+class Dog : Animal() {
+    fun bark() {
+        Log.d(LOG_TAG, "I am ${javaClass.simpleName}, I can bark.")
     }
 }
 
+class Human : Animal(), Speakable {
+    override fun speak() {
+        Log.d(LOG_TAG, "I am ${javaClass.simpleName}, I can speak.")
+    }
+}
+
+open class Robot : Moveable {
+    override fun move() {
+        Log.d(LOG_TAG, "I am ${javaClass.simpleName}, I can move.")
+    }
+}
+
+class TalkingRobot : Robot(), Speakable {
+    override fun speak() {
+        Log.d(LOG_TAG, "I am ${javaClass.simpleName}, I can speak.")
+    }
+}
