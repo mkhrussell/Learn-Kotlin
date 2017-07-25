2ab3b6068f8a42b6c88d75bfb3afd08848a2ebb9
Mohammad Kamrul Hasan <mkhrussell@gmail.com>
2017-07-25 23:46:03 +0600

    Class: Enum class

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/MainActivity.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/MainActivity.kt
@@ -4,6 +4,19 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 
+enum class Answers {
+    YES, NO, NOT_ANSWERED
+}
+
+enum class States {
+    SUCCEEDED,
+    FAILED
+}
+
+enum class Weekdays {
+    MON, TUE, WED, THU, FRI, SAT, SUN
+}
+
 class MainActivity : AppCompatActivity() {
 
     private val TAG: String = javaClass.simpleName
@@ -13,49 +26,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //Log.d(TAG, "MyDbConnection: dbName = ${MyDbConnection.dbName}, isOpen = ${MyDbConnection.isOpen()}")
+        Log.d(TAG, giveAnswer(Answers.NO))
+        Log.d(TAG, giveAnswer(Answers.YES))
+        Log.d(TAG, giveAnswer(Answers.NOT_ANSWERED))
 
-        //var cookieOne = Cookie()
-        //var cookieTwo = Cookie.create()
+        if (checkState(49) == States.FAILED) Log.d(TAG, "FAILED")
+        if (checkState(51) == States.SUCCEEDED) Log.d(TAG, "SUCCEEDED")
 
-        //Log.d(TAG, "Cookie: myName = ${Cookie.myName}")
-
-        var cookieThree = Cookie.Factory.create("Cookie Three")
-        Log.d(TAG, "cookieThree: name = ${cookieThree.name}")
-
-        var cookieFour = Cookie.Factory.create("Cookie Four")
-        Log.d(TAG, "cookieThree: name = ${cookieFour.name}")
+        for (day in Weekdays.values()) {
+            Log.d(TAG, "${day.name} = ${day.ordinal}")
+        }
 
         Log.d(TAG, "onCreate: ends")
     }
-}
 
-//open class DbConnection(dbName: String) {
-//    var dbName: String = dbName
-//}
-//
-//
-//object MyDbConnection: DbConnection("connection1") {
-//    fun isOpen() = true
-//}
-
-//class Cookie {
-//    companion object {
-//        fun create() = Cookie()
-//        var myName: String = javaClass.simpleName
-//    }
-//}
-
-class Cookie {
-    private constructor()
-
-    var name: String = ""
-
-    companion object Factory {
-        fun create(name: String): Cookie {
-            var ret = Cookie()
-            ret.name = name
-            return ret
+    fun giveAnswer(ans: Answers): String {
+        when (ans) {
+            Answers.YES -> return "Answered: Yes"
+            Answers.NO -> return "Answered: No"
+            Answers.NOT_ANSWERED -> return "Not answered"
         }
+    }
+
+    fun checkState(value: Int): States {
+        if (value <= 50) return States.FAILED
+        else return States.SUCCEEDED
     }
 }
