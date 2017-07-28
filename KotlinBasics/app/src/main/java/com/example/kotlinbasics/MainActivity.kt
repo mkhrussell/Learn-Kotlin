0ec3ec7f275064619f1c6daad65980087ecc4533
Mohammad Kamrul Hasan <mkhrussell@gmail.com>
2017-07-28 23:11:28 +0600

    Lambda: Lambda Expressions and inline functions

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/MainActivity.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/MainActivity.kt
@@ -6,39 +6,70 @@ import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
-    private val TAG: String = javaClass.simpleName
+    public val TAG: String = javaClass.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d(TAG, "onCreate: starts")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var personOne = Person("Russell", 30)
+        doSomething() // Inline function call
 
-        personOne++
-        personOne++
+        debugPrint("Welcome from normal function.")
 
-        Log.d(TAG, "Current age of ${personOne.name} is: ${personOne.age}")
+        pringString("Welcome from Higher Order Funstion.", this::debugPrint) // Higher order function
 
-        personOne--
+        pringString("Welcome from anonymous function.",
+                fun(message: String): Unit { // Anonymous function
+                    Log.d(TAG, message)
+                }
+        )
 
-        Log.d(TAG, "Current age of ${personOne.name} is: ${personOne.age}")
+        pringString("Welcome from Lambda expression # 1", { message: String -> Log.d(TAG, message) }) // Lambda expression # 1
+
+        pringString("Welcome from Lambda expression # 2", { message -> Log.d(TAG, message) })
+
+        pringString("Welcome from Lambda expression # 3", { Log.d(TAG, it) }) // implicit name of single parameter
+
+        val numbers = listOf<Int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+        var sumOfEvens = 0
+        numbers.filter { it % 2 == 0 }.forEach {
+            // Use lambda as closure
+            sumOfEvens += it
+        }
+
+        val returnedFunc = twoParamFunction({
+            Log.d(TAG, "From param 1 Lambda")
+        }, {
+            Log.d(TAG, "From param 2 Lambda which will be returned")
+        })
+
+        returnedFunc()
+
+        Log.d(TAG, "sumOfEvens = $sumOfEvens")
 
         Log.d(TAG, "onCreate: ends")
     }
-}
 
-data class Person(var name: String, var age: Int) {
-    operator fun inc(): Person {
-        this.age++
+    // Function Type: fun1: (param1: T1) -> T2
+    // Higher order functions take another funcion as parameter
 
-        return this
+    inline fun pringString(str: String, execFun: (msg: String) -> Unit) {
+        return execFun(str)
     }
 
-    operator fun dec(): Person {
-        this.age--
-
-        return this
+    inline fun twoParamFunction(func1: () -> Unit, noinline func2: () -> Unit): () -> Unit {
+        func1()
+        return func2
     }
 
+    fun debugPrint(msg: String): Unit {
+
+        Log.d(TAG, msg)
+    }
+
+    inline fun doSomething() {
+        Log.d(TAG, "Doing something.")
+    }
 }
