581d271fb5557f9c3dd385d64e5d1d4b36ba888d
Mohammad Kamrul Hasan <mkhrussell@gmail.com>
2017-07-29 00:42:38 +0600

    Extension Functions

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/MainActivity.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/MainActivity.kt
@@ -13,63 +13,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        doSomething() // Inline function call
+        val original = 2000
+        var addedTwo = original.plusTwo()
 
-        debugPrint("Welcome from normal function.")
+        Log.d(TAG, "$addedTwo")
 
-        pringString("Welcome from Higher Order Funstion.", this::debugPrint) // Higher order function
-
-        pringString("Welcome from anonymous function.",
-                fun(message: String): Unit { // Anonymous function
-                    Log.d(TAG, message)
-                }
-        )
-
-        pringString("Welcome from Lambda expression # 1", { message: String -> Log.d(TAG, message) }) // Lambda expression # 1
-
-        pringString("Welcome from Lambda expression # 2", { message -> Log.d(TAG, message) })
-
-        pringString("Welcome from Lambda expression # 3", { Log.d(TAG, it) }) // implicit name of single parameter
-
-        val numbers = listOf<Int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-
-        var sumOfEvens = 0
-        numbers.filter { it % 2 == 0 }.forEach {
-            // Use lambda as closure
-            sumOfEvens += it
-        }
-
-        val returnedFunc = twoParamFunction({
-            Log.d(TAG, "From param 1 Lambda")
-        }, {
-            Log.d(TAG, "From param 2 Lambda which will be returned")
-        })
-
-        returnedFunc()
-
-        Log.d(TAG, "sumOfEvens = $sumOfEvens")
+        val myCLass1 = MyClass()
+        myCLass1.internalFunc()
+        myCLass1.internalFunc(200)
 
         Log.d(TAG, "onCreate: ends")
     }
+}
 
-    // Function Type: fun1: (param1: T1) -> T2
-    // Higher order functions take another funcion as parameter
+fun Int.plusTwo() = this + 2
 
-    inline fun pringString(str: String, execFun: (msg: String) -> Unit) {
-        return execFun(str)
+class MyClass {
+    fun internalFunc() {
+        Log.d("MainActivity", "I am internal function.")
     }
+}
 
-    inline fun twoParamFunction(func1: () -> Unit, noinline func2: () -> Unit): () -> Unit {
-        func1()
-        return func2
-    }
-
-    fun debugPrint(msg: String): Unit {
-
-        Log.d(TAG, msg)
-    }
-
-    inline fun doSomething() {
-        Log.d(TAG, "Doing something.")
-    }
+fun MyClass.internalFunc(value: Int) {
+    Log.d("MainActivity", "I am extension function. Value = $value")
 }
