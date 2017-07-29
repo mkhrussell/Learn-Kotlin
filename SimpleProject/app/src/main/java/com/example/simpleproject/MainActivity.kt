884e614454544aea1530fab5204629c484681ac9
Mohammad Kamrul Hasan <mkhrussell@gmail.com>
2017-07-30 01:26:40 +0600

    Android Basics with Kotlin #01 - Create a Simple Project using Android Studio

--- SimpleProject/app/src/main/java/com/example/simpleproject/MainActivity.kt
+++ SimpleProject/app/src/main/java/com/example/simpleproject/MainActivity.kt
@@ -1,19 +1,12 @@
-package com.example.kotlinbasics
+package com.example.simpleproject
 
-import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
-    public val TAG: String = javaClass.simpleName
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d(TAG, "onCreate: starts")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        Log.d(TAG, "onCreate: ends")
     }
-
 }
