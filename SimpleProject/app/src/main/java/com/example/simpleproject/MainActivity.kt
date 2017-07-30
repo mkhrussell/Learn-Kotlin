39313ef57a2b17a0a060d1370ce2858493b57423
Mohammad Kamrul Hasan <mkhrussell@gmail.com>
2017-07-30 23:48:28 +0600

    Android Basics with Kotlin #02 - Debugging using Android Studio

--- SimpleProject/app/src/main/java/com/example/simpleproject/MainActivity.kt
+++ SimpleProject/app/src/main/java/com/example/simpleproject/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.simpleproject
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val numbers = arrayOf(1, 2, 3, 4, 5)
+
+        var sum = 0
+        var index = 0
+
+        while (index <= 4) {
+            sum += numbers[index]
+            index++
+        }
+
+        Log.d("MainActivity", "sum = $sum") // sum == 15
     }
 }
