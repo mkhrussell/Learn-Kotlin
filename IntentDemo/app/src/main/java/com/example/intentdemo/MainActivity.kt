940798453ab4a4d970a474250d6e7ba614f76472
Mohammad Kamrul Hasan <mkhrussell@gmail.com>
2017-08-08 00:44:05 +0600

    Android Basics with Kotlin #07 - Implicit Intents - Part #1 Starting app/activity with specific action

--- IntentDemo/app/src/main/java/com/example/intentdemo/MainActivity.kt
+++ IntentDemo/app/src/main/java/com/example/intentdemo/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.intentdemo
 
 import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
+        val TAG = "MainActivity"
         val MY_MESSAGE = "com.example.intentdemo.MY_MESSAGE"
     }
 
@@ -16,12 +20,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnSend = findViewById<Button>(R.id.btn_send)
-        btnSend.setOnClickListener {
-            val displayMessageIntent = Intent(applicationContext, DisplayMessageActivity::class.java)
-            val message = (findViewById<EditText>(R.id.edt_message)).text.toString()
-            displayMessageIntent.putExtra(MY_MESSAGE, message)
-            startActivity(displayMessageIntent)
+        (findViewById<Button>(R.id.btn_send)).setOnClickListener(buttonClickListener)
+        (findViewById<Button>(R.id.btn_implicit_telephone)).setOnClickListener(buttonClickListener)
+        (findViewById<Button>(R.id.btn_implicit_custom)).setOnClickListener(buttonClickListener)
+    }
+
+    private val buttonClickListener: (View) -> Unit = { view ->
+        when(view.id) {
+            R.id.btn_send -> {
+                val displayMessageIntent = Intent(applicationContext, DisplayMessageActivity::class.java)
+                val message = (findViewById<EditText>(R.id.edt_message)).text.toString()
+                displayMessageIntent.putExtra(MY_MESSAGE, message)
+                startActivity(displayMessageIntent)
+            }
+
+            R.id.btn_implicit_telephone -> {
+                val phoneNumber: Uri = Uri.parse("tel:+16465648568")
+                val dialIntent: Intent = Intent(Intent.ACTION_DIAL, phoneNumber)
+                startActivity(dialIntent)
+
+                Log.d(TAG, "btn_implicit_telephone: Clicked")
+            }
+
+            R.id.btn_implicit_custom -> {
+                val customIntent = Intent("dummy.intent.action.MY_ACTION")
+                startActivity(customIntent)
+
+                Log.d(TAG, "btn_implicit_custom: Clicked")
+            }
         }
+
     }
 }
